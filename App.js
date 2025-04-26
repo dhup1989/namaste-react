d71422cd06bac72ed5a31c11f008142b3d0fb9d4
dhupendrasingh <dhup1989@gmail.com>
2025-04-26 16:35:53 +0530

    Functional component

--- App.js
+++ App.js
@@ -1,40 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello react element"
-);
- */
+//const heading = React.createElement("h1", { id: "heading" }, "Namste react");
+// JSX: Easy to create element
 
-// Creating nested div, parent and child
+const heading = <h1 className="head">Namste react 🧨</h1>;
 
-/* const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is child h1 tag"),
-    React.createElement("h2", {}, "This is child h2 tag"),
-  ])
-); */
+const Title = () => {
+  return <h1>Title component</h1>;
+};
 
-// If we want wnat to one more child in parent
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is child h1 tag"),
-    React.createElement("h2", {}, "This is child h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is child2 h1 tag"),
-    React.createElement("h2", {}, "This is child2 h2 tag"),
-  ]),
-]);
-
-// Problem in this approach is this code looks messy, create problem in future.
-// Solution: JSX
+// React Functional component: It is normal java script function
+// Component composition: Component inside component
+// Here Title component inside HeadingComponent
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1 className="heading">Namaste react functional component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(parent);
+
+//root.render(heading);
+root.render(<HeadingComponent />);
